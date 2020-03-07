a9312d3e5c6faf04762ff1302235213c9b70061f
Thorsten Eurich <thorsten@eurich.de>
2020-03-07 09:34:32 +0100

    Initial commit

---
+++ SenseProntoImporter.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"net/url"
+	"html/template"
+	"encoding/json"
+	"strings"
+	"time"
+	"io/ioutil"
+)
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func toJSON(m interface{}) string {
+	js, err := json.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.ReplaceAll(string(js), ",", ", ")
+}
+
+func listIR (w http.ResponseWriter, r *http.Request) {
+	ip := r.URL.Query().Get("ip")
+	
+	sense := "http://"
+	sense += ip
+	sense += "/ir/list"
+ 
+	req, _ := http.NewRequest("GET", sense, nil)
+ 
+	req.Header.Add("cache-control", "no-cache")
+ 
+	res, _ := http.DefaultClient.Do(req)
+ 
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+ 
+	fmt.Fprintf(w, string(body))
+}
+
+func addIR (w http.ResponseWriter, r *http.Request) {
+	ip := r.URL.Query().Get("ip")
+	id := r.URL.Query().Get("id")
+	name := r.URL.Query().Get("name")
+	pronto := r.URL.Query().Get("pronto")
+	
+	//$sense = 'http://' . $_GET['ip'] . '/ir/add/?id='. $_GET['id'] . '&name=' . urlencode($_GET['name']) . '&pronto='. $_GET['pronto'];
+	
+	sense := "http://"
+	sense += ip
+	sense += "/ir/add?id="
+	sense += id
+	sense += "&name="
+	sense += url.QueryEscape(name)
+	sense += "&pronto="
+	sense += pronto
+ 
+	req, _ := http.NewRequest("GET", sense, nil)
+ 
+	req.Header.Add("cache-control", "no-cache")
+ 
+	res, _ := http.DefaultClient.Do(req)
+ 
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+ 
+	fmt.Fprintf(w, string(body))
+}
+
+func tplIndex(w http.ResponseWriter, r *http.Request) {  
+  t, err := template.ParseFiles("index.html")
+    if err != nil {
+        fmt.Println(err)
+    }
+    items := struct {
+		Shelly string
+	}{
+        Shelly: "IP",
+    }
+    t.Execute(w, items)
+}
+
+func main() {
+    http.HandleFunc("/", tplIndex)
+	http.HandleFunc("/list", listIR)
+	http.HandleFunc("/add", addIR)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
